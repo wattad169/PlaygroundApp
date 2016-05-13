0356c2d9fc171ec751d0537c05d1efd8360e497d
idan narotzki <idanaroz@gmail.com>
2016-05-13 13:04:29 +0300

    (check push )

--- app/src/main/java/com/inc/playground/playground/Home.java
+++ app/src/main/java/com/inc/playground/playground/Home.java
@@ -40,7 +40,7 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+/*idan check*/
 
 //
 //        menuButtonOpen = (Button) findViewById(R.id.settings_menu);
