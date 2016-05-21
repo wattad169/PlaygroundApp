70d7e179ea66072cd7fb8fcf7f164831b6104860
mostafawattad <wattad169@gmail.com>
2016-05-21 18:23:04 +0300

    Menu in all activites and createevent works and new filter screen can be pressed by clicking on MyProfile in the menu

--- app/src/main/java/com/inc/playground/playground/FilterActivity.java
+++ app/src/main/java/com/inc/playground/playground/FilterActivity.java
@@ -1,5 +1,8 @@
 package com.inc.playground.playground;
 
+import android.app.ActionBar;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.app.Activity;
 import android.preference.Preference;
@@ -8,10 +11,15 @@ import android.preference.PreferenceFragment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
-public class FilterActivity extends PreferenceActivity {
+import com.inc.playground.playground.utils.DownloadImageBitmapTask;
 
+import java.util.concurrent.ExecutionException;
+
+public class FilterActivity extends PreferenceActivity {
+    public static final String TAG = "EventInfoActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +59,45 @@ public class FilterActivity extends PreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
+        }
+    }
+
+    public void setPlayGroundActionBar(){
+        String userLoginId,userFullName,userEmail,userPhoto;
+        Bitmap imageBitmap =null;
+        GlobalVariables globalVariables;
+        final ActionBar actionBar = getActionBar();
+        final String MY_PREFS_NAME = "Login";
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        globalVariables = ((GlobalVariables) this.getApplication());
+        if (prefs.getString("userid", null) != null){
+            userLoginId = prefs.getString("userid", null);
+            userFullName = prefs.getString("fullname", null);
+            userEmail = prefs.getString("emilid", null);
+            userPhoto = prefs.getString("picture", null);
+            actionBar.setCustomView(R.layout.actionbar_custom_view_home);
+            actionBar.setDisplayShowTitleEnabled(true);
+            actionBar.setDisplayShowCustomEnabled(true);
+            actionBar.setDisplayUseLogoEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            ImageView img_profile = (ImageView) findViewById(R.id.img_profile_action_bar);
+            imageBitmap = globalVariables.GetUserPictureBitMap();
+            if(imageBitmap==null){
+                Log.i(TAG,"downloading");
+                try {
+                    imageBitmap = new DownloadImageBitmapTask().execute(userPhoto).get();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            else {
+                Log.i(TAG,"Image found");
+            }
+            img_profile.setImageBitmap(imageBitmap);
+            globalVariables.SetUserPictureBitMap(imageBitmap); // Make the imageBitMap global to all activities to avoid downloading twice
         }
     }
 
