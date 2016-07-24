a0aca6be1a352a3e65128d46494b7b118499274d
idan narotzki <idanaroz@gmail.com>
2016-07-24 15:18:42 +0300

    version 5.1

---
+++ app/src/main/java/com/inc/playground/playgroundApp/upLeft3StripesButton/MyEvents.java
@@ -0,0 +1,12 @@
+package com.inc.playground.playgroundApp.upLeft3StripesButton;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by idanaroz on 13-May-16.
+ *  The package refer all the screens that opened from the upper left button, with the 3 stripes
+ */
+public class MyEvents extends AppCompatActivity {
+
+
+}
