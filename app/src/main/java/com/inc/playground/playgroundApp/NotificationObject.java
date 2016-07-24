141c17a187d0b962e6ee6232e48bf4fc2e16cc7a
Lina <linagitovich@gmail.com>
2016-07-24 16:12:58 +0300

    version 5.0

---
+++ app/src/main/java/com/inc/playground/playgroundApp/NotificationObject.java
@@ -0,0 +1,54 @@
+package com.inc.playground.playgroundApp;
+
+import org.json.JSONObject;
+
+/**
+ * Created by lina on 7/13/2016.
+ */
+public class NotificationObject {
+    EventsObject event;
+    String description, type, title;
+    JSONObject inputJson;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public EventsObject getEvent() {
+        return event;
+    }
+
+    public void setEvent(EventsObject event) {
+        this.event = event;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    public JSONObject getInputJson() {
+        return inputJson;
+    }
+
+    public void setInputJson(JSONObject inputJson) {
+        this.inputJson = inputJson;
+    }
+}
+
+
